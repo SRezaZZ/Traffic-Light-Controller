f4d147bfafdbe8c5b6d9f88793419ec45909e693
SRezzaZZ <srezazz.mail@gmail.com>
2026-08-18 05:57:58 +0330

    refactor: finaliza timer

--- src/timer.v
+++ src/timer.v
@@ -1,24 +1,23 @@
-module timer (
+module timer(
     input clock,
-    input standby_i,
-    input [25:0] max_time,
+    input [31:0] max_time,
     output reg done
 );
 
-reg [25:0] counter = 0;
+reg [31:0] counter;
 
 always @(posedge clock) begin
-    if (standby_i) begin
-        counter <= 0;
-        done <= 0;
+    if(max_time==0) begin
+        counter<=0;
+        done<=0;
     end
-    else if (counter == max_time - 1) begin
-        counter <= 0;
-        done <= 1;
+    else if(counter>=max_time-1) begin
+        counter<=0;
+        done<=1;
     end
     else begin
-        counter <= counter + 1;
-        done <= 0;
+        counter<=counter+1;
+        done<=0;
     end
 end
 
