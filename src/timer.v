83705255cc1c2403b9361e7774f9ecc038b56759
SRezzaZZ <srezazz.mail@gmail.com>
2026-08-19 07:11:39 +0330

    feat: add timer reset

--- src/timer.v
+++ src/timer.v
@@ -1,5 +1,6 @@
-module timer(
+module timer (
     input clock,
+    input reset_i,
     input [31:0] max_time,
     output reg done
 );
@@ -7,17 +8,21 @@ module timer(
 reg [31:0] counter;
 
 always @(posedge clock) begin
-    if(max_time==0) begin
-        counter<=0;
-        done<=0;
+    if (reset_i) begin
+        counter <= 0;
+        done <= 0;
     end
-    else if(counter>=max_time-1) begin
-        counter<=0;
-        done<=1;
+    else if (max_time == 0) begin
+        counter <= 0;
+        done <= 0;
+    end
+    else if (counter >= max_time - 1) begin
+        counter <= 0;
+        done <= 1;
     end
     else begin
-        counter<=counter+1;
-        done<=0;
+        counter <= counter + 1;
+        done <= 0;
     end
 end
 
